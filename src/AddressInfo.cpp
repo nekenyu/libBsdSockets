b273709648455b444b50f50e19e0c29f4589267f
Jonathan Seng <jonathan.seng@komododoes.com>
2013-03-22 13:50:57 -0700

    AddressInfo: Replace if/elses with c++ {} initialized maps

--- src/AddressInfo.cpp
+++ src/AddressInfo.cpp
@@ -1,40 +1,68 @@
 #include <sys/socket.h>
 #include <stdexcept>
 
+#include <map>
+
 #include "AddressInfo.h"
 
 namespace BsdSockets {
 
   int socketDomainToRaw(SocketDomain domain) {
-    if(SocketDomain::INET4 == domain) { return PF_INET; }
-    else if(SocketDomain::INET6 == domain) { return PF_INET6; }
-    else if(SocketDomain::LOCAL == domain) { return PF_LOCAL; }
-    else if(SocketDomain::ROUTE == domain) { return PF_ROUTE; }
-    else if(SocketDomain::KEY == domain) { return PF_KEY; }
-    else if(SocketDomain::SYSTEM == domain) { return PF_SYSTEM; }
-    else if(SocketDomain::RAW == domain) { return PF_NDRV; }
-    else throw std::invalid_argument("Unknown SocketDomain");
+    static const std::map<SocketDomain, int> map =
+      {
+        {SocketDomain::INET4, PF_INET },
+        {SocketDomain::INET6, PF_INET6 },
+        {SocketDomain::LOCAL, PF_LOCAL },
+        {SocketDomain::ROUTE, PF_ROUTE },
+        {SocketDomain::KEY, PF_KEY },
+        {SocketDomain::SYSTEM, PF_SYSTEM },
+        {SocketDomain::RAW, PF_NDRV }
+      };
+
+    const auto found = map.find(domain);
+    if(found == map.end()) {
+      throw std::invalid_argument("Unknown SocketDomain");
+    }
+
+    return found->second;
   }
 
   SocketDomain rawToSocketDomain(int raw) {
-    if(PF_INET == raw) { return SocketDomain::INET4; }
-    else if(PF_INET6 == raw) { return SocketDomain::INET6; }
-    else if(PF_LOCAL == raw) { return SocketDomain::LOCAL; }
-    else if(PF_ROUTE == raw) { return SocketDomain::ROUTE; }
-    else if(PF_KEY == raw) { return SocketDomain::KEY; }
-    else if(PF_SYSTEM == raw) { return SocketDomain::SYSTEM; }
-    else if(PF_NDRV == raw) { return SocketDomain::RAW; }
-    else throw std::invalid_argument("Unknown SocketDomain");
+    static const std::map<int, SocketDomain> map =
+      {
+        {PF_INET, SocketDomain::INET4 },
+        {PF_INET6, SocketDomain::INET6 },
+        {PF_LOCAL, SocketDomain::LOCAL },
+        {PF_ROUTE, SocketDomain::ROUTE },
+        {PF_KEY, SocketDomain::KEY },
+        {PF_SYSTEM, SocketDomain::SYSTEM },
+        {PF_NDRV, SocketDomain::RAW }
+      };
+
+    const auto found = map.find(raw);
+    if(found == map.end()) {
+      throw std::invalid_argument("Unknown SocketDomain");
+    }
+
+    return found->second;
   }
 
 
   int socketTypeToRaw(SocketType type) {
-    if(SocketType::STREAM == type) { return SOCK_STREAM; }
-    else if(SocketType::DATAGRAM == type) { return SOCK_DGRAM; }
-    else if(SocketType::RAW_PROTOCOL == type) { return SOCK_RAW; }
-    else if(SocketType::RELIABLY_DELIVERED == type) { return SOCK_RDM; }
-    else if(SocketType::SEQUENCED_PACKET == type) { return SOCK_SEQPACKET; }
-    else throw std::invalid_argument("Unknown SocketType");
+    static const std::map<SocketType, int> map =
+      {
+	{SocketType::STREAM, SOCK_STREAM},
+	{SocketType::DATAGRAM, SOCK_DGRAM},
+	{SocketType::RAW_PROTOCOL, SOCK_RAW},
+	{SocketType::RELIABLY_DELIVERED, SOCK_RDM},
+	{SocketType::SEQUENCED_PACKET, SOCK_SEQPACKET}
+      };
+
+    const auto found = map.find(type);
+    if(found == map.end()) {
+      throw std::invalid_argument("Unknown SocketType");
+    }
+    return found->second;
   }
 
 } // namespace BsdSockets
